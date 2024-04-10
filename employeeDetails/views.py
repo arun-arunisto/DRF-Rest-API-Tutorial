773e7b3d950d923bb5d2b19174e23a24400b9ec0
arun-arunisto <arun.arunisto2@gmail.com>
2024-04-10 12:22:23 +0530

    nic-cbe employee details 10.04.2024

--- employeeDetails/views.py
+++ employeeDetails/views.py
@@ -2,6 +2,7 @@ from .models import Employee
 from .serializer import EmployeeSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 # Create your views here.
 @api_view(['GET', 'POST']) #this decorator only used when we use function based views
@@ -10,13 +11,13 @@ def employee_list(request):
         serializer = EmployeeSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     all_emp = Employee.objects.all()
     #converting to json using serializer
     serialized_data = EmployeeSerializer(all_emp, many=True) #if we are fetching more than one data we need to add many=True
-    return Response(serialized_data.data)
+    return Response(serialized_data.data, status=status.HTTP_200_OK) #status code 200
 
 @api_view(["GET", "PUT", "DELETE"])
 def employee_details(request, id):
@@ -25,15 +26,15 @@ def employee_details(request, id):
         serializer = EmployeeSerializer(emp, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     if request.method == "DELETE":
         emp = Employee.objects.get(id=id)
         emp.delete()
-        return Response({"message":"Success"})
+        return Response({"message":"Success"}, status=status.HTTP_200_OK)
     emp = Employee.objects.get(id=id)
     serialized_data = EmployeeSerializer(emp)
-    return Response(serialized_data.data)
+    return Response(serialized_data.data, status=status.HTTP_200_OK)
 
 
