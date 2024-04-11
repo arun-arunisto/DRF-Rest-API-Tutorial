d4b98ae370f41ba3ffdef0b126f675d7c85755d1
arun-arunisto <arun.arunisto2@gmail.com>
2024-04-11 13:47:49 +0530

    nic-cbe class based views 11.04.2024

--- TodoApp/urls.py
+++ TodoApp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TodoListView
+from .views import TodoListView, TodoDetailView
 
 urlpatterns = [
-    path('todo-list/', TodoListView.as_view(), name="todo-list")
+    path('todo-list/', TodoListView.as_view(), name="todo-list"),
+    path('todo-details/<int:id>', TodoDetailView.as_view(), name='todo-details')
 ]
--- TodoApp/views.py
+++ TodoApp/views.py
@@ -18,3 +18,21 @@ class TodoListView(APIView):
             return Response(serialized_data.data, status=status.HTTP_201_CREATED)
         return Response(serialized_data.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class TodoDetailView(APIView):
+    def get(self, request, id):
+        data = Todo.objects.get(id=id)
+        serialized_data = TodoSerializer(data)
+        return Response(serialized_data.data, status=status.HTTP_200_OK)
+
+    def put(self, request, id):
+        t_data = Todo.objects.get(id=id)
+        serialized_data = TodoSerializer(t_data, data=request.data)
+        if serialized_data.is_valid():
+            serialized_data.save()
+            return Response(serialized_data.data, status=status.HTTP_200_OK)
+        return Response(serialized_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        data = Todo.objects.get(id=id)
+        data.delete()
+        return Response({"message":"Deleted Successfully"}, status=status.HTTP_200_OK)
