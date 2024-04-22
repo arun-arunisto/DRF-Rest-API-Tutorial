4b6aee0aa66ded8372fb40bdc5f0f0f6f3c5c2cb
arun-arunisto <arun.arunisto2@gmail.com>
2024-04-22 13:59:20 +0530

    nic-cbe 22.04.2024

--- votersDetails/serializer.py
+++ votersDetails/serializer.py
@@ -5,3 +5,10 @@ class VotersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Voters
         fields = "__all__"
+
+    #field-level validations
+    def validate_age(self, value):
+        if value < 18:
+            raise serializers.ValidationError("Age must be greater than 18")
+        return value
+
