0baf102fbb0707a5c33b8072f0b2cc15dc27e44c
arun-arunisto <arun.arunisto2@gmail.com>
2024-04-23 11:20:06 +0530

    nic-cbe 23.04.2024

--- votersDetails/serializer.py
+++ votersDetails/serializer.py
@@ -1,19 +1,32 @@
 from rest_framework import serializers
 from .models import Voters
 
+#validators
+def age_valid(value):
+    if value < 18:
+        raise serializers.ValidationError("Age must be greater than 18")
+    return value
+
+def voter_id_valid(value):
+    if len(value) > 15:
+        raise serializers.ValidationError("Voter id not valid")
+    return value
 class VotersSerializer(serializers.ModelSerializer):
+    name = serializers.CharField()
+    age = serializers.IntegerField(validators=[age_valid])
+    voter_id_no = serializers.CharField(validators=[voter_id_valid])
     class Meta:
         model = Voters
         fields = "__all__"
 
     #field-level validations
-    def validate_age(self, value):
+    """def validate_age(self, value):
         if value < 18:
             raise serializers.ValidationError("Age must be greater than 18")
-        return value
+        return value"""
 
     #object-level validation
-    def validate(self, data):
+    """def validate(self, data):
         if len(data["voter_id_no"]) > 15:
             raise serializers.ValidationError("Invalid Voter id")
-        return data
+        return data"""
