96e6b632d082a53e2977dc57753c86dfd1b74626
arun-arunisto <arun.arunisto2@gmail.com>
2024-04-23 12:12:03 +0530

    nic-cbe 23.04.2024

--- votersDetails/serializer.py
+++ votersDetails/serializer.py
@@ -15,10 +15,15 @@ class VotersSerializer(serializers.ModelSerializer):
     name = serializers.CharField()
     age = serializers.IntegerField(validators=[age_valid])
     voter_id_no = serializers.CharField(validators=[voter_id_valid])
+
+    #serializer method field
+    len_voter_id_no = serializers.SerializerMethodField()
     class Meta:
         model = Voters
         fields = "__all__"
 
+    def get_len_voter_id_no(self, object):
+        return len(object.voter_id_no)
     #field-level validations
     """def validate_age(self, value):
         if value < 18:
