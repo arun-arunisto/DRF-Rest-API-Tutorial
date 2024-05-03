4b5aad25b47030a723f452caa22d9596cb0f53ea
arun-arunisto <arun.arunisto2@gmail.com>
2024-05-03 14:15:05 +0530

    nic-cbe 03.05.2024

--- BlogApp/urls.py
+++ BlogApp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BlogDetailView
+from .views import BlogDetailView, BlogListView
 
 urlpatterns = [
-    path('blog-detail-view/', BlogDetailView.as_view(), name='blog-detail-view')
+    path('blog-detail-view/', BlogDetailView.as_view(), name='blog-detail-view'),
+    path('blog-list-view/<int:id>/', BlogListView.as_view(), name='blog-list-view')
 ]
--- BlogApp/views.py
+++ BlogApp/views.py
@@ -18,3 +18,22 @@ class BlogDetailView(APIView):
             return Response(serialized_data.data, status=status.HTTP_200_OK)
         return Response(serialized_data.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class BlogListView(APIView):
+    def get(self, request, id):
+        data = Blog.objects.get(id=id)
+        serialized_data = BlogSerializer(data)
+        return Response(serialized_data.data, status=status.HTTP_200_OK)
+
+    def put(self, request, id):
+        data = Blog.objects.get(id=id)
+        serialized_data = BlogSerializer(data, request.data)
+        if serialized_data.is_valid():
+            serialized_data.save()
+            return Response(serialized_data.data, status=status.HTTP_200_OK)
+        return Response(serialized_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        data = Blog.objects.get(id=id)
+        data.delete()
+        return Response({"Message":"Deleted Successfully!!"}, status=status.HTTP_200_OK)
+
