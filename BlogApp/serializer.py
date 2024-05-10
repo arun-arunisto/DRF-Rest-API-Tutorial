3208987c67b7b472ba6c827e60aaeb9df70b6ca2
arun-arunisto <arun.arunisto2@gmail.com>
2024-05-10 14:08:06 +0530

    nic-cbe 10.05.2024

--- BlogApp/serializer.py
+++ BlogApp/serializer.py
@@ -14,9 +14,10 @@ class CategorySerializer(serializers.ModelSerializer):
     #API Reference
     #category = serializers.StringRelatedField(many=True)
     #category = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-    category = serializers.HyperlinkedRelatedField(
+    """category = serializers.HyperlinkedRelatedField(
         many=True,
-        read_only=True, view_name='blog-list-view', lookup_field='id')
+        read_only=True, view_name='blog-list-view', lookup_field='id')"""
+    category = serializers.SlugRelatedField(many=True, read_only=True, slug_field='slug')
 
     class Meta:
         model = Category
