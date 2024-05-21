d7b3b563f34c2f9edba14535695790446589d373
arun-arunisto <arun.arunisto2@gmail.com>
2024-05-21 14:49:44 +0530

    nic-cbe 21.05.2024

--- MusicAPI/views.py
+++ MusicAPI/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from rest_framework import viewsets
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from .models import *
 from .serializer import *
 
@@ -11,10 +12,22 @@ class TrackViewSet(viewsets.ViewSet):
         serialized_data = TrackSerializer(queryset, many=True)
         return Response(serialized_data.data)
 
+    def retrieve(self, request, pk=None):
+        queryset = Track.objects.all()
+        track = get_object_or_404(queryset, pk=pk)
+        serialized_data = TrackSerializer(track)
+        return Response(serialized_data.data)
+
 class AlbumViewSet(viewsets.ViewSet):
     def list(self, request):
         queryset = Album.objects.all()
         serialized_data = AlbumSerializer(queryset, many=True)
+        return Response(serialized_data.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Album.objects.all()
+        album = get_object_or_404(queryset, pk=pk)
+        serialized_data = AlbumSerializer(album)
         return Response(serialized_data.data)
 
 class ArtistViewSet(viewsets.ViewSet):
@@ -22,6 +35,13 @@ class ArtistViewSet(viewsets.ViewSet):
         queryset = Artist.objects.all()
         serialized_data = ArtistSerializer(queryset, many=True)
         return Response(serialized_data.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Artist.objects.all()
+        artist = get_object_or_404(queryset, pk=pk)
+        serialized_data = ArtistSerializer(artist)
+        return Response(serialized_data.data)
+
 
 
 
