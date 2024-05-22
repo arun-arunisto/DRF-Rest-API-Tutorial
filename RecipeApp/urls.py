ef094eac4a0a23d3b1e90fbb285166ed43b816e7
arun-arunisto <arun.arunisto2@gmail.com>
2024-05-22 12:44:49 +0530

    nic-cbe 22.05.2024

--- RecipeApp/urls.py
+++ RecipeApp/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path, include
-from rest_framework import routers
 from .views import *
 
-router = routers.DefaultRouter()
-router.register(r'chefs', viewset=ChefModelView, basename="chefs")
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('recipe-list-create-view/', RecipeListCreateView.as_view(), name='recipe-list-create-view'),
+    path('chef-list-create-view/', ChefListCreateView.as_view(), name="chef-list-create-view")
 ]
--- RecipeApp/views.py
+++ RecipeApp/views.py
@@ -1,11 +1,30 @@
-from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import generics
 from .models import *
 from .serializer import *
 
+class RecipeListCreateView(generics.ListCreateAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
 
-# Create your views here.
-class ChefModelView(viewsets.ModelViewSet):
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = RecipeSerializer(queryset, many=True)
+        if queryset.exists():
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({"message":"No data available"}, status=status.HTTP_204_NO_CONTENT)
+
+
+class ChefListCreateView(generics.ListCreateAPIView):
     queryset = Chef.objects.all()
     serializer_class = ChefSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = ChefSerializer(queryset, many=True)
+        if queryset.exists():
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({"message":"No data available"}, status=status.HTTP_204_NO_CONTENT)
