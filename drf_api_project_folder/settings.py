175b7d587d7e35a16af852990c891fb9df05b04d
arun-arunisto <arun.arunisto2@gmail.com>
2024-05-24 10:15:38 +0530

    nic-cbe 24.05.2024

--- drf_api_project_folder/settings.py
+++ drf_api_project_folder/settings.py
@@ -134,3 +134,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
