8a7409f66de9094cb1a700b1c009bbb0f687aeeb
arun-arunisto <arun.arunisto2@gmail.com>
2024-05-26 12:15:32 +0530

    personal pc commit 26.05.2024

--- LbraryApp/urls.py
+++ LbraryApp/urls.py
@@ -3,5 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('book-list-view/', book_list, name='book-list-view'),
-    path('book-detail-view/<int:id>/', book_details, name='book-detail-view')
+    path('book-detail-view/<int:id>/', book_details, name='book-detail-view'),
+    path('reader-list-view/', ReaderListView, name='reader-list-view'),
+    path('reader-detail-view/<int:id>/', ReaderDetailView, name='reader-detail-view')
 ]
--- LbraryApp/views.py
+++ LbraryApp/views.py
@@ -21,6 +21,7 @@ def book_list(request):
     return Response(serialized_data.data, status=status.HTTP_200_OK)
 
 @api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAdminUserOrReadOnly])
 def book_details(request, id):
     if request.method == 'PUT':
         book_data = Book.objects.get(id=id)
@@ -40,6 +41,38 @@ def book_details(request, id):
     serialized_data = BookSerializer(book_data)
     return Response(serialized_data.data, status=status.HTTP_200_OK)
 
+@api_view(['GET', 'POST'])
+def ReaderListView(request):
+    if request.method == "POST":
+        serialized_data = ReaderSerializer(data=request.data)
+        if serialized_data.is_valid():
+            serialized_data.save()
+            return Response(serialized_data.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serialized_data.errors, status=status.HTTP_400_BAD_REQUEST)
+    reader_data = Reader.objects.all()
+    serialized_data = ReaderSerializer(reader_data, many=True)
+    return Response(serialized_data.data, status=status.HTTP_200_OK)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def ReaderDetailView(request, id):
+    if request.method == 'PUT':
+        reader_data = Reader.objects.get(id=id)
+        serialized_data = ReaderSerializer(reader_data, data=request.data)
+        if serialized_data.is_valid():
+            serialized_data.save()
+            return Response(serialized_data.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serialized_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'DELETE':
+        reader_data = Reader.objects.get(id=id)
+        reader_data.delete()
+        return Response({"message":"Deleted Successfully!"}, status=status.HTTP_200_OK)
+
+    reader_data = Reader.objects.get(id=id)
+    serialized_data = ReaderSerializer(reader_data)
+    return Response(serialized_data.data, status=status.HTTP_200_OK)
 
 
 
