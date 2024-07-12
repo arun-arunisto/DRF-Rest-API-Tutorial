13dd7150edb1f8d43ff6dd1004ac00b57d3f53e9
arun-royalbrothers <arun.a@royalbrothers.com>
2024-07-12 14:43:58 +0530

    celery redis

--- advancedMethods/serializer.py
+++ advancedMethods/serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import *
+from .tasks import subscription_terminate_worker
 
 class LogCredSerializers(serializers.ModelSerializer):
     class Meta:
@@ -88,3 +89,11 @@ class PremiumSubscriptionSerializer(serializers.ModelSerializer):
         model = PremiumSubscription
         fields = "__all__"
 
+    def create(self, validated_data):
+        premium = PremiumSubscription(**validated_data)
+        premium_user = PremiumUsers.objects.get(id=premium.user.id)
+        premium_user.premium_status = True
+        premium_user.save()
+        premium.save()
+        subscription_terminate_worker.delay(premium_user.id)
+        return premium
--- advancedMethods/tasks.py
+++ advancedMethods/tasks.py
@@ -2,8 +2,37 @@
 #adding a simple task for testing
 #task 1: Sending a mail in every hour
 from django.core.mail import send_mail
+#import the required modules for celery
+from celery import shared_task
+from .models import PremiumSubscription, PremiumUsers
+from datetime import datetime
 
 
 #task 1
-def send_mail_in_every_one_hr():
+def send_mail_in_every_one_hr(id):
     send_mail("TEST MAIL", "test mail for django", "arun.a@royalbrothers.com",["arun.arunisto2@gmail.com"])
+
+
+@shared_task
+def subscription_terminate_worker(id): #worker
+    try:
+        end_time = PremiumSubscription.objects.filter(user__id=id, user__premium_status=True).order_by("-id").first().end_date
+        time_count = (end_time - datetime.now()).total_seconds()
+        if time_count > 0:
+            print("Task is added to queue")
+            subscription_termination.apply_async(args=[id], countdown=time_count)
+            print("Task scheduled successfully")
+    except PremiumSubscription.DoesNotExist:
+        print("Data does not exist")
+    except Exception as e:
+        print(e)
+
+
+@shared_task
+def subscription_termination(id): #job
+    premium_user = PremiumUsers.objects.get(id=id)
+    premium_user.premium_status = False
+    premium_user.save()
+    print("Task executed successfully")
+
+
